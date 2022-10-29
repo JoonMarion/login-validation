1501cd213fb39297ee39ae240d29d879bbf726bb
joonmarion <marianojoao52@gmail.com>
2022-10-29 08:41:48 -0300

    feat: dynamic button disabled

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,17 +1,20 @@
 import styled from 'styled-components';
 
-
 export const ButtonContainer = styled.button`
     width: 100%;
     height: 42px;
-    background-color: #81259D;
-    color: #FFF;
+    background-color: #81259d;
+    color: #fff;
 
-    border: 1px solid #81259D;
+    border: 1px solid #81259d;
     border-radius: 21px;
 
-    &:hover {
+    &:hover:enabled {
         opacity: 0.6;
-        cursor:pointer;
+        cursor: pointer;
     }
-`
+
+    &:disabled {
+        border: 1px solid red;
+    }
+`;
